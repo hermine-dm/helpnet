d5e0366aa465c69ee3b72eec352636fe7d3b7915
hermine-dm <herdementh@gmail.com>
2019-12-03 10:11:14 +0100

    [FINISH-DEBUG] -user-helper bf action done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	include ApplicationHelper
+	before_action :configure_devise_parameters, if: :devise_controller?
+
+	def configure_devise_parameters
+	  devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :password_confirmation)}
+	  devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:name, :first_name, :last_name, :description, :email, :zip_code, :password, :password_confirmation, :current_password)}
+	end
 end
