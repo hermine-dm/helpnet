8f90b040dc4408af7851887ffcd6aa6d587173a1
philippegatti <ph.gatti@gmail.com>
2019-12-04 10:29:25 +0100

    [progress] mailer devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "helpnet_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {:host => 'https://helpnet-dev.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'https://helpnet-dev.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
