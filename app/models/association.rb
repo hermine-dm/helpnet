33b295cc57f4656208b613d043bb0478b7312080
philippegatti <ph.gatti@gmail.com>
2019-12-05 15:47:08 +0100

    [create] model follow - controller & settings to be done

--- app/models/association.rb
+++ app/models/association.rb
@@ -1,9 +1,11 @@
 class Association < ApplicationRecord
-  	has_one_attached :logo
 	validates :name, presence: true, uniqueness: true
 	validates :description, presence: true, length: { in: 100..200, wrong_length: "100 to 200 characters is allowed" }
 	validates :zip_code, presence: true, format: { with: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/, message: "please enter a valid french zip code" }
 	validates :location, presence: true
 	validates :num_rna, presence: true, format: { with: /\A([W]([0-9]{9}))\z/, message: "please enter a valid french association num W+9nbs"}
 	#validates :email, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+  	has_one_attached :logo
+  	
+
 end
---
+++ db/migrate/20191205144220_create_follows.rb
@@ -0,0 +1,9 @@
+class CreateFollows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :follows do |t|
+    	t.belongs_to :user, index: true
+    	t.belongs_to :association, index: true
+    	t.timestamps
+    end
+  end
+end
