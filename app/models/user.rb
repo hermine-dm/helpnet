21140bde8b8f9cd61c9f8a155817a3bb0afc24b3
hermine-dm <herdementh@gmail.com>
2019-12-06 17:54:48 +0100

    [DECOMMENT]-mailjet-

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_many :follows
   has_many :organizations, through: :follows
   after_create :generate_name
-  #after_create :welcome_send
+  after_create :welcome_send
 
   def generate_name #create a name from the email adress and id
   	a = self.email.chars.index("@") #find the position of @ in the email adress
