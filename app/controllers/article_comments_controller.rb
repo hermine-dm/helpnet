2444c9b28d5da6dcf72ac517b152162eedd27b8c
philippegatti <ph.gatti@gmail.com>
2019-12-06 12:35:46 +0100

    [create] forms to add comments on article#show

--- app/controllers/article_comments_controller.rb
+++ app/controllers/article_comments_controller.rb
@@ -1,2 +1,8 @@
 class ArticleCommentsController < ApplicationController
+	before_action :authenticate_user
+	def create
+		@article=Article.find(params[:article_id])
+		@comment=ArticleComment.create(user_id:current_user.id, article_id:@article.id, content: params[:content])
+		redirect_back(fallback_location: article_path(@article.id))
+	end
 end
