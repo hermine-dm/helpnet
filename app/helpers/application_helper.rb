e1c1caa49a5a4554baa268aaa237260eca88f359
philippegatti <ph.gatti@gmail.com>
2019-12-06 15:11:53 +0100

    [progress] flash messages created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,5 +27,13 @@ module ApplicationHelper
 	    	redirect_to root_path
   		end
   	end
-
+  	
+	def bootstrap_class_for_flash(type)
+	  case type
+	    when 'notice' then "alert-info"
+	    when 'success' then "alert-success"
+	    when 'error' then "alert-danger"
+	    when 'alert' then "alert-warning"
+	  end
+	end
 end
