7880b6c7077e73e4590dd7c3bca813ccf8017691
philippegatti <ph.gatti@gmail.com>
2019-12-09 18:13:43 +0100

    [in progress] events#edit view created but not operational

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,12 @@
 class EventsController < ApplicationController
-	before_action :authenticate_user
-	before_action :address_validation, only: [:create,:edit]
+	before_action :authenticate_user, except: [:show, :index]
+	before_action :address_validation, only: [:create]
 
 	def new
 		@organization=Organization.find(params[:organization_id])
 	end
 	def index
-		@events=Event.all
+		@events=Event.where(organization_id:params[:organization_id])
 	end
 	def show
 		@organization=Organization.find(params[:organization_id])
@@ -27,9 +27,24 @@ class EventsController < ApplicationController
 	def edit
 		@organization=Organization.find(params[:organization_id])
 		@event=Event.find(params[:id])
+		@adress=Address.find(@event.address_id)
 	end
 	def update
+		@event = Event.find(params[:id])
+		@address = Address.find(@event.address_id)
+	    if (@event.update(start_date: "#{params[:start_date]} #{params[:start_time]}",end_date: "#{params[:end_date]} #{params[:end_time]}",title: params[:title], description:params[:description], illustration: params[:illustration]) && @address.update(number: params[:number], street:params[:street], additionnal_information:params[:additionnal_information], zip_code:params[:zip_code], city:params[:city]))
+	      flash[:success] = "Les informations ont bien été prises en compte"
+	      redirect_to organization_event_path(@organization.id,@event.id)
+	    else
+	      flash.now[:error] = "Désolé il y a une erreur :#{@event.errors.full_messages.to_sentence} #{@address.errors.full_messages.to_sentence}"
+	      render :edit
+	    end
 	end
 	def destroy
+		@event=Event.find(params[:id])
+		@organization=Organization.find(params[:organization_id])
+		@event.destroy
+		flash[:success] = "L'évènement a été supprimé"
+    	redirect_to organization_path(@organization.id)
 	end
 end
