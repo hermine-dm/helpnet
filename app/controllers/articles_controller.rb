646ced884403c00bf461c1891f4ded4f4b37cc3d
philippegatti <ph.gatti@gmail.com>
2019-12-09 11:09:46 +0100

    [create] view new#article created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,14 @@ class ArticlesController < ApplicationController
 		@articles=Article.all
 	end
 	def create
+		@article=Article.new(user_id: current_user.id, title: params[:title], content: params[:content])
+		if @article.save
+			flash[:success] = "Article créé - Merci pour votre contribution!" 
+      		redirect_to article_path(@article.id)
+    	else
+		    flash.now[:error] = "Désolé il y a une erreur :#{@article.errors.full_messages.to_sentence}"
+		    render :new
+    	end
 	end
 	def edit
 	end
