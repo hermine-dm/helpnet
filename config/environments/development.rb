d0f2663f8d0a69a5d48906c059e5e56d166a8b55
hermine-dm <herdementh@gmail.com>
2019-12-11 13:10:57 +0100

    [FINISH] seed done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #active_storage seed
+  config.active_job.queue_adapter = :inline
 end
