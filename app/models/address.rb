02890106f7c27af971b53f0eed955742645a3e4f
philippegatti <ph.gatti@gmail.com>
2019-12-11 13:54:21 +0100

    [finish] static gooogle map for events +event controller#update fixed

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,9 @@
 class Address < ApplicationRecord
 	validates :zip_code, presence: true, format: { with: /\A(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}\z/, message: "please enter a valid french zip code" }
 	has_many :events
+
+	def google_format
+		@address="#{self.number}+#{self.street}+#{self.zip_code}+#{self.city}+France"
+		return @address.tr(" ","")
+	end
 end
