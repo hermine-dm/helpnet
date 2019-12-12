3ff725a3c0f23ed15e50f2f35ff42170472259c0
philippegatti <ph.gatti@gmail.com>
2019-12-12 21:29:04 +0100

    debug thu 9:28pm

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -64,6 +64,7 @@ class OrganizationsController < ApplicationController
   def destroy
     @organization = Organization.friendly.find_by_slug(params[:slug])
     @address = Address.find(@organization.address_id)
+    @organization.events.destroy_all
     @organization.follows.destroy_all
     @organization.destroy
     @address.destroy
