3dbffff149db7cf5f7538551d470d77178df9931
hermine-dm <herdementh@gmail.com>
2019-12-12 18:53:28 +0100

    [FINISH] ajax

--- app/controllers/article_likes_controller.rb
+++ app/controllers/article_likes_controller.rb
@@ -1,14 +1,22 @@
 class ArticleLikesController < ApplicationController
 	before_action :authenticate_user, only: [:create, :destroy]
+	
 	def create
-		@article=Article.find(params[:article_id])
-		@like=ArticleLike.create(user_id:current_user.id, article_id:@article.id)
-		redirect_back(fallback_location: article_path(@article.id))
+		@article = Article.find(params[:article_id])
+		@like = ArticleLike.create(user_id:current_user.id, article_id:@article.id)
+		respond_to do |format|
+        	format.html { redirect_back(fallback_location: article_path(@article.id))}
+        	format.js { }
+      	end
 	end
+
 	def destroy
-		@article=Article.find(params[:article_id])
-		@like=@article.article_likes.find(params[:id])
+		@article = Article.find(params[:article_id])
+		@like = @article.article_likes.find(params[:id])
 		@like.delete
-		redirect_back(fallback_location: article_path(params[:article_id]))
+		respond_to do |format|
+        	format.html { redirect_back(fallback_location: article_path(params[:article_id]))}
+        	format.js { }
+      	end
 	end
 end
--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,15 +1,24 @@
 class FollowsController < ApplicationController
 	before_action :authenticate_user, only: [:create, :destroy]
+
 	def create
-		@follow=Follow.create(user_id:current_user.id, organization_id:params[:organization_id])
-		flash[:success] = "Vous suivez desormais #{Organization.find(params[:organization_id]).name}"
-		redirect_back(fallback_location: organizations_path)
+		@organization = Organization.find(params[:organization_id])
+		@follow = Follow.create(user_id:current_user.id, organization_id: @organization.id)
+		#flash.now[:success] = "Vous suivez desormais #{@organization.name}"
+		respond_to do |format|
+        	format.html { redirect_back(fallback_location: organizations_path)}
+        	format.js { }
+      	end
 	end
+
 	def destroy
-		@organization=Organization.find(params[:organization_id])
-		@follow=@organization.follows.find(params[:id])
+		@organization = Organization.find(params[:organization_id])
+		@follow = @organization.follows.find(params[:id])
 		@follow.delete
-		flash[:error] = "Vous ne suivez plus #{Organization.find(params[:organization_id]).name}"
-		redirect_back(fallback_location: organizations_path)
+		#flash.now[:error] = "Vous ne suivez plus #{Organization.find(params[:organization_id]).name}"
+		respond_to do |format|
+        	format.html { redirect_back(fallback_location: organizations_path)}
+        	format.js { }
+      	end
 	end
 end
