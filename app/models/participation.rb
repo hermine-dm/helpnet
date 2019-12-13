e43653260dfb205a8035e006bb7c2aaa03863252
philippegatti <ph.gatti@gmail.com>
2019-12-13 11:17:23 +0100

    mailers events creation for followers and event_confirmation for participants [created]

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,12 @@
 class Participation < ApplicationRecord
 	belongs_to :user
 	belongs_to :event
+	after_create :event_confirmation_send
+
+	def event_confirmation_send
+		@event=Event.find(self.event_id)
+		@user=User.find(self.user_id)
+		@organization = Organization.find(@event.organization_id)
+   		UserMailer.event_confirmation_email(@user,@event,@organization).deliver_now
+    end
 end
